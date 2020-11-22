9a62253cf551f3a3d09321d0dccaa0b059530d69
Robert Lai <rcylai@hotmail.com>
2020-11-22 12:31:05 -0800

    Pics of playing any hls stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import Hls from 'hls.js';
 
 function App() {
+  const inputRef: React.RefObject<HTMLInputElement> = useRef(null);
+  const videoRef: React.RefObject<HTMLVideoElement> = useRef(null);
+  const [streamUrl, setStreamUrl] = useState('');
+
+  useEffect(() => {
+    if (!videoRef || !videoRef.current || !streamUrl) return;
+    const hls = new Hls();
+    hls.loadSource(streamUrl);
+    hls.attachMedia(videoRef.current);
+
+    hls.on(Hls.Events.MANIFEST_PARSED, () => {
+      videoRef && videoRef.current && videoRef.current.play();
+    });
+  }, [streamUrl]);
+
+  const setStream = () => {
+    if (!inputRef || !inputRef.current) return;
+    setStreamUrl(inputRef.current.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContainer">
+      <div className="videoContainer">
+        <video className="video" ref={videoRef} autoPlay={true} />
+      </div>
+      <div className="inputBar">
+        <input className="input" ref={inputRef} />
+        <button onClick={setStream}>Set stream</button>
+      </div>
     </div>
   );
 }
