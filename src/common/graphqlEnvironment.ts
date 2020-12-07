ca751cdff5cb64c1ebf499e1fd4381a33273b8d2
Connor Worley <cworley@dropbox.com>
2020-12-06 21:06:52 -0800

    fix graphql fetch on phones

--- src/common/graphqlEnvironment.ts
+++ src/common/graphqlEnvironment.ts
@@ -1,16 +1,21 @@
 import { Environment, Network, RecordSource, Store } from "relay-runtime";
 
 function fetchQuery(operation: any, variables: any) {
-  return fetch("http://localhost:8080/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      query: operation.text,
-      variables,
-    }),
-  }).then((response) => {
+  return fetch(
+    window.location.port === "8080"
+      ? "/graphql"
+      : "http://localhost:8080/graphql",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: operation.text,
+        variables,
+      }),
+    }
+  ).then((response) => {
     return response.json();
   });
 }
