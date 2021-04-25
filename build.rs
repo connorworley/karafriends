01b34775106003e07a02a957491964d002bcd6e5
Connor Worley <connorbworley@gmail.com>
2021-04-25 16:26:16 -0700

    cargo fmt/clippy

--- build.rs
+++ build.rs
@@ -3,16 +3,15 @@ pub fn main() {
 
     #[cfg(windows)]
     {
+        let mut copy_options = fs_extra::dir::CopyOptions::new();
+        copy_options.overwrite = true;
+        fs_extra::dir::copy("./prebuilt/windows", &out_dir, &copy_options).unwrap();
+
         println!("cargo:rustc-link-search={}/windows", out_dir);
+
         println!("cargo:rustc-link-lib=libaubio-5");
         println!("cargo:rustc-link-lib=libfftw3f-3");
         println!("cargo:rustc-link-lib=libwinpthread-1");
-
-        let mut copy_options = fs_extra::dir::CopyOptions::new();
-        copy_options.overwrite = true;
-        fs_extra::dir::copy("./prebuilt/windows", out_dir, &copy_options).unwrap();
-
-        
 
         windows::build!(
             Windows::Foundation::IAsyncOperation,
