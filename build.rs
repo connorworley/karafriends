19697646aea2825f7a4cc64f004b15aa2b4a5de7
Connor Worley <connorbworley@gmail.com>
2021-04-25 16:19:05 -0700

    prebuild aubio/fftw for windows

--- build.rs
+++ build.rs
@@ -1,18 +1,31 @@
 pub fn main() {
-    println!("cargo:rustc-link-lib=aubio");
+    let out_dir = std::env::var("OUT_DIR").unwrap();
 
     #[cfg(windows)]
-    windows::build!(
-        Windows::Foundation::IAsyncOperation,
-        Windows::Foundation::Collections::IVector,
-        Windows::Foundation::EventRegistrationToken,
-        Windows::Foundation::IMemoryBufferReference,
-        Windows::Foundation::TypedEventHandler,
-        Windows::Media::*,
-        Windows::Media::Audio::*,
-        Windows::Media::Capture::*,
-        Windows::Media::MediaProperties::*,
-        Windows::Media::Render::*,
-        Windows::Win32::WinRT::IMemoryBufferByteAccess,
-    );
+    {
+        println!("cargo:rustc-link-search={}/windows", out_dir);
+        println!("cargo:rustc-link-lib=libaubio-5");
+        println!("cargo:rustc-link-lib=libfftw3f-3");
+        println!("cargo:rustc-link-lib=libwinpthread-1");
+
+        let mut copy_options = fs_extra::dir::CopyOptions::new();
+        copy_options.overwrite = true;
+        fs_extra::dir::copy("./prebuilt/windows", out_dir, &copy_options).unwrap();
+
+        
+
+        windows::build!(
+            Windows::Foundation::IAsyncOperation,
+            Windows::Foundation::Collections::IVector,
+            Windows::Foundation::EventRegistrationToken,
+            Windows::Foundation::IMemoryBufferReference,
+            Windows::Foundation::TypedEventHandler,
+            Windows::Media::*,
+            Windows::Media::Audio::*,
+            Windows::Media::Capture::*,
+            Windows::Media::MediaProperties::*,
+            Windows::Media::Render::*,
+            Windows::Win32::WinRT::IMemoryBufferByteAccess,
+        );
+    }
 }
