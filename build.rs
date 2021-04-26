926d20358d23cb9f60cd2c4b6e50d01c3409c095
Connor Worley <connorbworley@gmail.com>
2021-04-25 21:31:12 -0700

    ensure prebuild works on windows, update readme

--- build.rs
+++ build.rs
@@ -11,17 +11,9 @@ pub fn main() {
 
     #[cfg(windows)]
     {
-        // I could not for the life of me get static libraries working on Windows
-        let out_dir = std::env::var("OUT_DIR").unwrap();
-        let mut copy_options = fs_extra::dir::CopyOptions::new();
-        copy_options.overwrite = true;
-        fs_extra::dir::copy("./prebuilt/windows", &out_dir, &copy_options).unwrap();
-
-        println!("cargo:rustc-link-search={}/windows", out_dir);
-
-        println!("cargo:rustc-link-lib=libaubio-5");
-        println!("cargo:rustc-link-lib=libfftw3f-3");
-        println!("cargo:rustc-link-lib=libwinpthread-1");
+        println!("cargo:rustc-link-search=./prebuilt/windows");
+        println!("cargo:rustc-link-lib=static=libaubio");
+        println!("cargo:rustc-link-lib=static=libfftw3f");
 
         windows::build!(
             Windows::Foundation::IAsyncOperation,
