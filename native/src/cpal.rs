c09b38e03ee14f261abd37ede76589a868251f89
Connor Worley <connorbworley@gmail.com>
2021-04-28 23:35:04 -0700

    Fix cpal issues on windoze

--- native/src/cpal.rs
+++ native/src/cpal.rs
@@ -1,8 +1,20 @@
 use std::sync::{Arc, Mutex};
 
-use cpal::traits::{DeviceTrait, HostTrait};
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use neon::prelude::*;
 
+fn windows_safe_call<T: Send + 'static>(f: impl FnOnce() -> T + Send + 'static) -> T {
+    #[cfg(windows)]
+    {
+        let (tx, rx) = std::sync::mpsc::channel();
+        std::thread::spawn(move || {
+            tx.send(f()).unwrap();
+        });
+        rx.recv().unwrap()
+    }
+    #[cfg(not(windows))]
+    f()
+}
 pub struct AudioSystem {}
 pub struct InputDevice {
     _stream: Arc<Mutex<cpal::platform::Stream>>,
@@ -17,59 +29,68 @@ impl crate::traits::AudioSystemTrait for AudioSystem {
     type InputDevice = InputDevice;
 
     fn input_devices() -> Vec<String> {
-        let host = cpal::default_host();
-        host.input_devices()
-            .unwrap()
-            .map(|device| device.name().unwrap())
-            .collect()
+        windows_safe_call(|| {
+            let host = cpal::default_host();
+            host.input_devices()
+                .unwrap()
+                .map(|device| device.name().unwrap())
+                .collect()
+        })
     }
 
     fn output_devices() -> Vec<String> {
-        let host = cpal::default_host();
-        host.output_devices()
-            .unwrap()
-            .map(|device| device.name().unwrap())
-            .collect()
+        windows_safe_call(|| {
+            let host = cpal::default_host();
+            host.output_devices()
+                .unwrap()
+                .map(|device| device.name().unwrap())
+                .collect()
+        })
     }
 
     fn new_input_device(name: &str) -> Self::InputDevice {
-        let host = cpal::default_host();
-        let device = host
-            .input_devices()
-            .unwrap()
-            .find(|device| device.name().unwrap() == name)
-            .unwrap();
-        let config = device.default_input_config().unwrap();
+        let name = name.to_string();
+        windows_safe_call(move || {
+            let host = cpal::default_host();
+            let device = host
+                .input_devices()
+                .unwrap()
+                .find(|device| device.name().unwrap() == name)
+                .unwrap();
+            let config = device.default_input_config().unwrap();
 
-        let sample_rate = config.sample_rate().0;
-        let sample_count = (sample_rate / 40) as usize;
+            let sample_rate = config.sample_rate().0;
+            let sample_count = (sample_rate / 40) as usize;
 
-        let (mut tx, rx) = ringbuf::RingBuffer::new(sample_count).split();
+            let (mut tx, rx) = ringbuf::RingBuffer::new(sample_count).split();
 
-        let pitch_detector = aubio_rs::Pitch::new(
-            aubio_rs::PitchMode::Yinfast,
-            sample_count,
-            sample_count,
-            sample_rate,
-        )
-        .unwrap();
-
-        let stream = device
-            .build_input_stream(
-                &config.into(),
-                move |samples: &[f32], _| {
-                    tx.push_iter(&mut samples.iter().copied());
-                },
-                |e| std::panic!("{}", e),
+            let pitch_detector = aubio_rs::Pitch::new(
+                aubio_rs::PitchMode::Yinfast,
+                sample_count,
+                sample_count,
+                sample_rate,
             )
             .unwrap();
 
-        InputDevice {
-            _stream: Arc::new(Mutex::new(stream)),
-            rx,
-            sample_count,
-            pitch_detector: Arc::new(Mutex::new(pitch_detector)),
-        }
+            let stream = device
+                .build_input_stream(
+                    &config.into(),
+                    move |samples: &[f32], _| {
+                        tx.push_iter(&mut samples.iter().copied());
+                    },
+                    |e| std::panic!("{}", e),
+                )
+                .unwrap();
+
+            stream.play().unwrap();
+
+            InputDevice {
+                _stream: Arc::new(Mutex::new(stream)),
+                rx,
+                sample_count,
+                pitch_detector: Arc::new(Mutex::new(pitch_detector)),
+            }
+        })
     }
 }
 
