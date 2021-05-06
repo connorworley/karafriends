8783049402b4d31b4d5f26922b1e7b2fdb30c730
Connor Worley <connorbworley@gmail.com>
2021-05-05 21:32:04 -0700

    remove unused file

--- src/renderer/window.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-  interface Window {
-    karafriends: {
-      hasCredentials(): Promise<boolean>;
-      setCredentials(username: string, password: string): void;
-    };
-  }
-}
