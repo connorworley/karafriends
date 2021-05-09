b3c20bf18f457d5a009404f8d3a57e53051ef222
Connor Worley <connorbworley@gmail.com>
2021-05-08 23:04:15 -0700

    add midi number to Y coord helper function

--- src/renderer/PianoRoll.tsx
+++ src/renderer/PianoRoll.tsx
@@ -1,4 +1,4 @@
- /* tslint:disable:max-classes-per-file */
+/* tslint:disable:max-classes-per-file */
 
 import React, { useEffect, useRef, useState } from "react";
 
@@ -38,14 +38,7 @@ function quadToTriangles(x0: number, y0: number, x1: number, y1: number) {
 
     GL makes us list triangles in counter-clockwise order
   */
-  return [
-    x0, y0,
-    x0, y1,
-    x1, y1,
-    x0, y0,
-    x1, y1,
-    x1, y0,
-  ];
+  return [x0, y0, x0, y1, x1, y1, x0, y0, x1, y1, x1, y0];
 }
 
 function median(nums: number[]) {
@@ -71,21 +64,27 @@ abstract class ShaderProgram<T extends unknown[]> {
     shaders: WebGLShader[],
     attributeNames: string[],
     uniformNames: string[],
-    bufferNames: string[],
+    bufferNames: string[]
   ) {
     this.gl = gl;
     this.program = gl.createProgram()!;
-    shaders.forEach(shader => gl.attachShader(this.program, shader));
+    shaders.forEach((shader) => gl.attachShader(this.program, shader));
     gl.linkProgram(this.program);
     this.attributeLocations = Object.fromEntries(
-      attributeNames.map(name => [name, gl.getAttribLocation(this.program, name)]),
-    )
+      attributeNames.map((name) => [
+        name,
+        gl.getAttribLocation(this.program, name),
+      ])
+    );
     this.uniformLocations = Object.fromEntries(
-      uniformNames.map(name => [name, gl.getUniformLocation(this.program, name)!]),
-    )
+      uniformNames.map((name) => [
+        name,
+        gl.getUniformLocation(this.program, name)!,
+      ])
+    );
     this.buffers = Object.fromEntries(
-      bufferNames.map(name => [name, gl.createBuffer()!]),
-    )
+      bufferNames.map((name) => [name, gl.createBuffer()!])
+    );
   }
 
   abstract draw(...args: T): void;
@@ -94,10 +93,7 @@ abstract class ShaderProgram<T extends unknown[]> {
 class NoteProgram extends ShaderProgram<[number, number]> {
   readonly triangleCount: number;
 
-  constructor(
-    gl: WebGLRenderingContext,
-    positions: number[],
-  ) {
+  constructor(gl: WebGLRenderingContext, positions: number[]) {
     super(
       gl,
       [
@@ -106,7 +102,7 @@ class NoteProgram extends ShaderProgram<[number, number]> {
       ],
       ["position"],
       ["time", "timeWidth", "canvasWidth"],
-      ["positions"],
+      ["positions"]
     );
     gl.bindBuffer(gl.ARRAY_BUFFER, this.buffers.positions);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
@@ -117,7 +113,14 @@ class NoteProgram extends ShaderProgram<[number, number]> {
     if (this.gl.CURRENT_PROGRAM !== this.program) {
       this.gl.useProgram(this.program);
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.positions);
-      this.gl.vertexAttribPointer(this.attributeLocations.position, 2, this.gl.FLOAT, false, 0, 0);
+      this.gl.vertexAttribPointer(
+        this.attributeLocations.position,
+        2,
+        this.gl.FLOAT,
+        false,
+        0,
+        0
+      );
       this.gl.enableVertexAttribArray(this.attributeLocations.position);
       this.gl.uniform1f(this.uniformLocations.timeWidth, TIME_WIDTH_SECS);
     }
@@ -132,10 +135,7 @@ class NoteProgram extends ShaderProgram<[number, number]> {
 class PitchProgram extends ShaderProgram<[number, number, number[]]> {
   readonly color: [number, number, number];
 
-  constructor(
-    gl: WebGLRenderingContext,
-    color: [number, number, number],
-  ) {
+  constructor(gl: WebGLRenderingContext, color: [number, number, number]) {
     super(
       gl,
       [
@@ -144,7 +144,7 @@ class PitchProgram extends ShaderProgram<[number, number, number[]]> {
       ],
       ["position"],
       ["time", "timeWidth", "color"],
-      ["positions"],
+      ["positions"]
     );
     this.color = color;
   }
@@ -153,7 +153,14 @@ class PitchProgram extends ShaderProgram<[number, number, number[]]> {
     if (this.gl.CURRENT_PROGRAM !== this.program) {
       this.gl.useProgram(this.program);
       this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffers.positions);
-      this.gl.vertexAttribPointer(this.attributeLocations.position, 2, this.gl.FLOAT, false, 0, 0);
+      this.gl.vertexAttribPointer(
+        this.attributeLocations.position,
+        2,
+        this.gl.FLOAT,
+        false,
+        0,
+        0
+      );
       this.gl.enableVertexAttribArray(this.attributeLocations.position);
       this.gl.uniform1f(this.uniformLocations.timeWidth, TIME_WIDTH_SECS);
       this.gl.uniform3fv(this.uniformLocations.color, this.color);
@@ -162,15 +169,27 @@ class PitchProgram extends ShaderProgram<[number, number, number[]]> {
     this.gl.uniform1f(this.uniformLocations.time, time);
     this.gl.uniform1f(this.uniformLocations.canvasWidth, canvasWidth);
 
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.DYNAMIC_DRAW);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array(positions),
+      this.gl.DYNAMIC_DRAW
+    );
     this.gl.drawArrays(this.gl.TRIANGLES, 0, positions.length / 2);
   }
+}
+
+function midiNumberToYCoord(midiNumber: number, medianMidiNumber: number) {
+  // We draw 21 rows behind the canvas, and we also want to be able to align
+  // notes in-between rows, so we have 41 positions. We want to return
+  // positions that correspond to the center of a bar or in-between two bars.
+  // If we're at the median MIDI number, we should be dead-center.
+  return 0.5 + (midiNumber - medianMidiNumber) / (41 + 1);
 }
 
 export default function PianoRoll(props: {
   scoringData: number[];
   videoRef: React.RefObject<HTMLVideoElement>;
-  mic: InputDevice | null,
+  mic: InputDevice | null;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const animationFrameRequestRef = useRef<number>(0);
@@ -204,84 +223,57 @@ export default function PianoRoll(props: {
       .map((note) =>
         quadToTriangles(
           note.startTime,
-          (((note.midiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-            nBars) +
-            2) *
-            barSize,
+          midiNumberToYCoord(note.midiNumber + 1, medianMidiNumber),
           note.endTime,
-          ((note.midiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-            nBars) *
-            barSize
+          midiNumberToYCoord(note.midiNumber - 1, medianMidiNumber)
         )
       )
       .flat();
 
-    const pitchDetectionMidiNumbers: number[] = [];
-    const pitchDetectionTimestamps: number[] = [];
+    let currentNoteIndex = 0;
+    let pitchOffset = 0;
+
     const pitchDetectionPositions: number[] = [];
 
     function pushPitchDetection(pitchMidiNumber: number) {
       if (!props.videoRef.current) return;
-      if (pitchDetectionMidiNumbers.length >= 200) {
-        pitchDetectionMidiNumbers.shift();
-        pitchDetectionTimestamps.shift();
+      if (pitchDetectionPositions.length >= 200 * 12) {
         for (let i = 0; i < 12; i++) {
           pitchDetectionPositions.shift();
         }
       }
 
-      const previousMidiNumber = pitchDetectionMidiNumbers[pitchDetectionMidiNumbers.length - 1];
-      const previousTimestamp = pitchDetectionTimestamps[pitchDetectionTimestamps.length - 1];
-
-      pitchDetectionMidiNumbers.push(pitchMidiNumber);
-      pitchDetectionTimestamps.push(props.videoRef.current.currentTime);
-
-      if (pitchDetectionMidiNumbers.length === 0 || props.videoRef.current.currentTime - previousTimestamp > 0.030) {
-        pitchDetectionPositions.push(
-          ...quadToTriangles(
-            props.videoRef.current.currentTime - 0.025,
-            (((pitchMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) +
-              1.5) *
-              barSize,
-            props.videoRef.current.currentTime,
-            (((pitchMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) + 0.5) *
-              barSize
-          )
+      pitchDetectionPositions.push(
+        ...quadToTriangles(
+          props.videoRef.current.currentTime - 0.025,
+          midiNumberToYCoord(pitchMidiNumber + 0.5, medianMidiNumber),
+          props.videoRef.current.currentTime,
+          midiNumberToYCoord(pitchMidiNumber - 0.5, medianMidiNumber)
         )
-      } else {
-        pitchDetectionPositions.push(
-          previousTimestamp,
-          (((previousMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) +
-              1.5) *
-              barSize,
-          previousTimestamp,
-          (((previousMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) + 0.5) *
-              barSize,
-          props.videoRef.current.currentTime,
-          (((pitchMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) + 0.5) *
-              barSize,
-          previousTimestamp,
-          (((previousMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) +
-              1.5) *
-              barSize,
-          props.videoRef.current.currentTime,
-          (((pitchMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) + 0.5) *
-              barSize,
-          props.videoRef.current.currentTime,
-          (((pitchMidiNumber - medianMidiNumber + Math.floor(nBars / 2)) %
-              nBars) +
-              1.5) *
-              barSize,
-        );
-      }
+      );
     }
+
+    const pollPitch = setInterval(() => {
+      if (!props.mic || !props.videoRef.current) return;
+      const { frequency, confidence } = props.mic.getPitch();
+      if (
+        confidence > 0.8 &&
+        frequency !== 0 &&
+        !props.videoRef.current.paused
+      ) {
+        while (
+          notes[currentNoteIndex + 1].startTime <
+          props.videoRef.current.currentTime
+        ) {
+          currentNoteIndex++;
+        }
+        const currentMidiNumber = notes[currentNoteIndex].midiNumber;
+        pitchOffset +=
+          Math.round((currentMidiNumber - (frequency + pitchOffset)) / 12) * 12;
+        console.log(currentMidiNumber, frequency, pitchOffset);
+        pushPitchDetection(frequency + pitchOffset);
+      }
+    }, 25);
 
     const gl = canvasRef.current.getContext("webgl", {
       premultipliedAlpha: false,
@@ -301,18 +293,11 @@ export default function PianoRoll(props: {
       gl.clear(gl.COLOR_BUFFER_BIT);
 
       if (positions.length > 0) {
-        noteProgram.draw(
-          time,
-          canvasWidth,
-        );
+        noteProgram.draw(time, canvasWidth);
       }
 
       if (pitchDetectionPositions.length > 0) {
-        pitchProgram.draw(
-          time,
-          canvasWidth,
-          pitchDetectionPositions,
-        );
+        pitchProgram.draw(time, canvasWidth, pitchDetectionPositions);
       }
 
       animationFrameRequestRef.current = window.requestAnimationFrame(draw);
@@ -331,14 +316,6 @@ export default function PianoRoll(props: {
 
     updateSize();
     window.addEventListener("resize", updateSize);
-
-    const pollPitch = setInterval(() => {
-      if (!props.mic || !props.videoRef.current) return;
-      const { frequency, confidence } = props.mic.getPitch();
-      if (confidence > 0.8 && frequency !== 0 && !props.videoRef.current.paused) {
-        pushPitchDetection(frequency + 12);
-      }
-    }, 25);
 
     return () => {
       cancelAnimationFrame(animationFrameRequestRef.current);
