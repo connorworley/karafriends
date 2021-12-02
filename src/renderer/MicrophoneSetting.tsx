4b3374f93ecc9b0097ecfc1b0a57e8e054f7e5a9
Connor Worley <connorbworley@gmail.com>
2021-12-01 17:09:41 -0800

    First attempt at input device channel selection (#72)
    * First attempt at input device channel selection
    
    * simplify channel selection stuff
    
    * ubs
    
    * ubs 2
    
    * ubs 3

--- src/renderer/MicrophoneSetting.tsx
+++ src/renderer/MicrophoneSetting.tsx
@@ -4,7 +4,7 @@ import React, { useEffect } from "react";
 import "./global";
 
 export default function MicrophoneSetting(props: {
-  onChange: (name: string) => void;
+  onChange: (name: string, channelSelection: number) => void;
   value: string;
 }) {
   useEffect(() => {
@@ -15,16 +15,29 @@ export default function MicrophoneSetting(props: {
     <div className="input-field">
       <select
         value={props.value}
-        onChange={(e) => props.onChange(e.target.value)}
+        onChange={(e) => {
+          const dataset = e.target.options[e.target.selectedIndex].dataset;
+          props.onChange(dataset.name!, parseInt(dataset.channel!, 10));
+        }}
       >
         <option value="" disabled={true}>
           Select a microphone
         </option>
-        {window.karafriends.nativeAudio.inputDevices().map((name) => (
-          <option value={name} key={name}>
-            {name}
-          </option>
-        ))}
+        {window.karafriends.nativeAudio
+          .inputDevices()
+          .map(([name, channelCount]) =>
+            [
+              <option data-name={name} data-channel={-1} key={`${name}_${-1}`}>
+                {`${name} (All Channels)`}
+              </option>,
+            ].concat(
+              [...Array(channelCount)].map((_, i) => (
+                <option data-name={name} data-channel={i} key={`${name}_${i}`}>
+                  {`${name} (Channel ${i})`}
+                </option>
+              ))
+            )
+          )}
       </select>
       <label>Microphone</label>
     </div>
