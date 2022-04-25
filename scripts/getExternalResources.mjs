e0abbde598a9274570c6594f2e13a63f9aaf3d1b
Andy Tran <andytran2@gmail.com>
2022-04-24 17:58:32 -0700

    Only download platform relevant external resources

--- scripts/getExternalResources.mjs
+++ scripts/getExternalResources.mjs
@@ -9,6 +9,7 @@ import { exec } from "child_process";
 
 const pathTo7zip = sevenBin.path7za;
 const extraResourcesDir = `${process.cwd()}/extraResources`;
+const maxMsToWaitForExtraction = 20000;
 
 async function fetchWithRetries(url, retries) {
   return fetch(url).then((res) => {
@@ -32,72 +33,33 @@ async function downloadFile(url, path) {
   });
 }
 
-async function getExternalResources() {
-  const externalResourceChecks = [
-    fs.existsSync(`${extraResourcesDir}/ytdlp/yt-dlp`),
+const winTasks = {
+  doChecks: () => [
     fs.existsSync(`${extraResourcesDir}/ytdlp/yt-dlp.exe`),
-    fs.existsSync(`${extraResourcesDir}/ytdlp/yt-dlp_macos`),
     fs.existsSync(`${extraResourcesDir}/ffmpeg/win/ffmpeg.exe`),
-    fs.existsSync(`${extraResourcesDir}/ffmpeg/macos/ffmpeg`),
-    fs.existsSync(`${extraResourcesDir}/ffmpeg/linux/ffmpeg`),
-  ];
-  if (externalResourceChecks.every((check) => check === true)) {
-    return;
-  }
-
-  const tmpDir = fs.mkdtempSync(
-    path.join(os.tmpdir(), "karafriends_getExternalResources")
-  );
-  await Promise.all([
-    fs.mkdir(`${tmpDir}/ffmpeg/win`, { recursive: true }, () => null),
-    fs.mkdir(`${tmpDir}/ffmpeg/macos`, { recursive: true }, () => null),
-    fs.mkdir(`${tmpDir}/ffmpeg/linux`, { recursive: true }, () => null),
-    fs.mkdir(`${extraResourcesDir}/ytdlp`, { recursive: true }, () => null),
-    fs.mkdir(
-      `${extraResourcesDir}/ffmpeg/win`,
-      { recursive: true },
-      () => null
-    ),
-    fs.mkdir(
-      `${extraResourcesDir}/ffmpeg/macos`,
-      { recursive: true },
-      () => null
-    ),
-    fs.mkdir(
-      `${extraResourcesDir}/ffmpeg/linux`,
-      { recursive: true },
-      () => null
-    ),
-  ]);
-
-  await Promise.all([
-    downloadFile(
-      "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp",
-      `${extraResourcesDir}/ytdlp/yt-dlp`
-    ),
-    downloadFile(
-      "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp.exe",
-      `${extraResourcesDir}/ytdlp/yt-dlp.exe`
-    ),
-    downloadFile(
-      "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp_macos",
-      `${extraResourcesDir}/ytdlp/yt-dlp_macos`
-    ),
-    downloadFile(
-      "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z",
-      `${tmpDir}/ffmpeg/win/ffmpeg.7z`
-    ),
-    downloadFile(
-      "https://evermeet.cx/ffmpeg/ffmpeg-5.0.1.zip",
-      `${tmpDir}/ffmpeg/macos/ffmpeg.zip`
-    ),
-    downloadFile(
-      "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
-      `${tmpDir}/ffmpeg/linux/ffmpeg.tar.xz`
-    ),
-  ]);
-
-  const getWinFfmpeg = async () => {
+  ],
+  prepareDirs: async (tmpDir) =>
+    Promise.all([
+      fs.mkdir(`${tmpDir}/ffmpeg/win`, { recursive: true }, () => null),
+      fs.mkdir(`${extraResourcesDir}/ytdlp`, { recursive: true }, () => null),
+      fs.mkdir(
+        `${extraResourcesDir}/ffmpeg/win`,
+        { recursive: true },
+        () => null
+      ),
+    ]),
+  getAssets: async (tmpDir) =>
+    Promise.all([
+      downloadFile(
+        "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp.exe",
+        `${extraResourcesDir}/ytdlp/yt-dlp.exe`
+      ),
+      downloadFile(
+        "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z",
+        `${tmpDir}/ffmpeg/win/ffmpeg.7z`
+      ),
+    ]),
+  extractAssets: async (tmpDir, hasFinishedExtracting) => {
     exec(
       `${pathTo7zip} e ${tmpDir}/ffmpeg/win/ffmpeg.7z -y -o${tmpDir}/ffmpeg/win/contents`,
       (error, stdout, stderr) => {
@@ -108,12 +70,42 @@ async function getExternalResources() {
             if (err) {
               console.error(error);
             }
+            hasFinishedExtracting[0] = true;
           }
         );
       }
     );
-  };
-  const getMacosFfmpeg = async () => {
+  },
+  setPermissions: () => null,
+};
+
+const macosTasks = {
+  doChecks: () => [
+    fs.existsSync(`${extraResourcesDir}/ytdlp/yt-dlp_macos`),
+    fs.existsSync(`${extraResourcesDir}/ffmpeg/macos/ffmpeg`),
+  ],
+  prepareDirs: async (tmpDir) =>
+    Promise.all([
+      fs.mkdir(`${tmpDir}/ffmpeg/macos`, { recursive: true }, () => null),
+      fs.mkdir(`${extraResourcesDir}/ytdlp`, { recursive: true }, () => null),
+      fs.mkdir(
+        `${extraResourcesDir}/ffmpeg/macos`,
+        { recursive: true },
+        () => null
+      ),
+    ]),
+  getAssets: async (tmpDir) =>
+    Promise.all([
+      downloadFile(
+        "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp_macos",
+        `${extraResourcesDir}/ytdlp/yt-dlp_macos`
+      ),
+      downloadFile(
+        "https://evermeet.cx/ffmpeg/ffmpeg-5.0.1.zip",
+        `${tmpDir}/ffmpeg/macos/ffmpeg.zip`
+      ),
+    ]),
+  extractAssets: async (tmpDir, hasFinishedExtracting) => {
     exec(
       `${pathTo7zip} e ${tmpDir}/ffmpeg/macos/ffmpeg.zip -y -o${tmpDir}/ffmpeg/macos/contents`,
       (error, stdout, stderr) => {
@@ -124,12 +116,45 @@ async function getExternalResources() {
             if (err) {
               console.error(error);
             }
+            hasFinishedExtracting[0] = true;
           }
         );
       }
     );
-  };
-  const getLinuxFfmpeg = async () => {
+  },
+  setPermissions: () => {
+    fs.chmodSync(`${extraResourcesDir}/ffmpeg/macos/ffmpeg`, "755");
+    fs.chmodSync(`${extraResourcesDir}/ytdlp/yt-dlp_macos`, "755");
+  },
+};
+
+const linuxTasks = {
+  doChecks: () => [
+    fs.existsSync(`${extraResourcesDir}/ytdlp/yt-dlp`),
+    fs.existsSync(`${extraResourcesDir}/ffmpeg/linux/ffmpeg`),
+  ],
+  prepareDirs: async (tmpDir) =>
+    Promise.all([
+      fs.mkdir(`${tmpDir}/ffmpeg/linux`, { recursive: true }, () => null),
+      fs.mkdir(`${extraResourcesDir}/ytdlp`, { recursive: true }, () => null),
+      fs.mkdir(
+        `${extraResourcesDir}/ffmpeg/linux`,
+        { recursive: true },
+        () => null
+      ),
+    ]),
+  getAssets: async (tmpDir) =>
+    Promise.all([
+      downloadFile(
+        "https://github.com/yt-dlp/yt-dlp/releases/download/2022.04.08/yt-dlp",
+        `${extraResourcesDir}/ytdlp/yt-dlp`
+      ),
+      downloadFile(
+        "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz",
+        `${tmpDir}/ffmpeg/linux/ffmpeg.tar.xz`
+      ),
+    ]),
+  extractAssets: async (tmpDir, hasFinishedExtracting) => {
     exec(
       `${pathTo7zip} e ${tmpDir}/ffmpeg/linux/ffmpeg.tar.xz -y -o${tmpDir}/ffmpeg/linux/xz`,
       (error, stdout, stderr) => {
@@ -143,14 +168,55 @@ async function getExternalResources() {
                 if (err) {
                   console.error(error);
                 }
+                hasFinishedExtracting[0] = true;
               }
             );
           }
         );
       }
     );
-  };
-  await Promise.all([getLinuxFfmpeg(), getMacosFfmpeg(), getWinFfmpeg()]);
+  },
+  setPermissions: () => {
+    fs.chmodSync(`${extraResourcesDir}/ffmpeg/linux/ffmpeg`, "755");
+    fs.chmodSync(`${extraResourcesDir}/ytdlp/yt-dlp`, "755");
+  },
+};
+
+async function getExternalResources(tasks) {
+  if (tasks.doChecks().every((check) => check === true)) {
+    return;
+  }
+  const tmpDir = fs.mkdtempSync(
+    path.join(os.tmpdir(), "karafriends_getExternalResources")
+  );
+  await tasks.prepareDirs(tmpDir);
+  await tasks.getAssets(tmpDir);
+
+  let hasFinishedExtracting = [false];
+  let msToWaitForExtraction = maxMsToWaitForExtraction;
+  await tasks.extractAssets(tmpDir, hasFinishedExtracting);
+  await new Promise(async () => {
+    while (msToWaitForExtraction > 0 && !hasFinishedExtracting[0]) {
+      await new Promise((r) => setTimeout(r, 200));
+      msToWaitForExtraction -= 200;
+    }
+    fs.rmdirSync(tmpDir, { recursive: true });
+    if (!hasFinishedExtracting[0]) {
+      console.error(
+        `Extracting ffmpeg did not complete after ${maxMsToWaitForExtraction} ms and was aborted!`
+      );
+      process.exit(1);
+    }
+    tasks.setPermissions();
+    process.exit(0);
+  });
 }
 
-await getExternalResources();
+const tasks =
+  process.platform === "win32"
+    ? winTasks
+    : process.platform === "darwin"
+    ? macosTasks
+    : linuxTasks;
+
+await getExternalResources(tasks);
