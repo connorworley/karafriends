b9d9070a9cb89c6856c02bef1061f2784cf5e721
Connor Worley <connorbworley@gmail.com>
2022-05-21 00:19:40 -0700

    Remove c dependencies (#86)
    * wip remove C dependencies
    
    * whops

--- third_party/windows.build.bat
+++
@@ -1,2 +0,0 @@
-docker build -t karafriends-third_party-windows -f windows.Dockerfile .
-docker run -v "%CD%:/code" -v "%CD%\..\prebuilt\windows:/out" karafriends-third_party-windows /code/windows.build.sh
