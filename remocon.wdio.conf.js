e100c4e68495389c1125fa41b961e88cf7fa73ea
Connor Worley <connorbworley@gmail.com>
2023-05-21 16:03:42 -0700

    Create sim device if desired runtime/type combo doesn't exist already

--- remocon.wdio.conf.js
+++ remocon.wdio.conf.js
@@ -1,13 +1,27 @@
 const { execFileSync } = require("child_process");
 
-function iOsSimDeviceUdidByDeviceTypeId(deviceTypeId) {
+const KARAFRIENDS_SIM_DEVICE_NAME = "_karafriends_sim_device";
+
+function findOrCreateAppleSimDeviceUdid(runtime, deviceType) {
   const simInfo = JSON.parse(
     execFileSync("xcrun", ["simctl", "list", "devices", "--json"])
   );
-  console.log(simInfo);
-  return Object.values(simInfo.devices)[0].filter(
-    (device) => device.deviceTypeIdentifier === deviceTypeId
-  )[0];
+  if (simInfo.devices[runtime]) {
+    const device = simInfo.devices[runtime].find(
+      (device) =>
+        device.deviceTypeIdentifier === deviceType &&
+        device.name === KARAFRIENDS_SIM_DEVICE_NAME
+    );
+    if (device) return device.udid;
+  }
+
+  return execFileSync("xcrun", [
+    "simctl",
+    "create",
+    KARAFRIENDS_SIM_DEVICE_NAME,
+    deviceType,
+    runtime,
+  ]);
 }
 
 exports.config = {
@@ -25,8 +39,9 @@ exports.config = {
         browserName: "Safari",
         platformName: "iOS",
         "safari:deviceType": "iPhone",
-        "safari:deviceUDID": iOsSimDeviceUdidByDeviceTypeId(
-          "com.apple.CoreSimulator.SimDeviceType.iPhone-14"
+        "safari:deviceUDID": findOrCreateAppleSimDeviceUdid(
+          "com.apple.CoreSimulator.SimRuntime.iOS-16-4",
+          "com.apple.CoreSimulator.SimDeviceType.iPhone-12"
         ),
         "safari:useSimulator": true,
       },
