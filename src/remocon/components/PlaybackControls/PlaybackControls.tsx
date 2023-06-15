71dd53a3c7ed2812dd73410800fc3b54b79758c8
Nolan Lum <nolan.lum@mongodb.com>
2023-06-15 12:54:15 -0700

    i'm a ui god

--- src/remocon/components/PlaybackControls/PlaybackControls.tsx
+++ src/remocon/components/PlaybackControls/PlaybackControls.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { MdPause, MdPlayArrow, MdReplay, MdSkipNext } from "react-icons/md";
 
 import usePlaybackState from "../../../common/hooks/usePlaybackState";
+import PitchControls from "../PitchControls/PitchControls";
 import * as styles from "./PlaybackControls.module.scss";
 
 const PlaybackControls = () => {
@@ -11,24 +12,27 @@ const PlaybackControls = () => {
   const disabled = !["PLAYING", "PAUSED"].includes(playbackState);
 
   return (
-    <div
-      className={classnames(styles.controls, { [styles.disabled]: disabled })}
-    >
-      <div onClick={() => setPlaybackState("RESTARTING")}>
-        <MdReplay />
-      </div>
+    <>
       <div
-        className={styles.playPause}
-        onClick={() =>
-          setPlaybackState(playbackState === "PAUSED" ? "PLAYING" : "PAUSED")
-        }
+        className={classnames(styles.controls, { [styles.disabled]: disabled })}
       >
-        {playbackState === "PLAYING" ? <MdPause /> : <MdPlayArrow />}
+        <div onClick={() => setPlaybackState("RESTARTING")}>
+          <MdReplay />
+        </div>
+        <div
+          className={styles.playPause}
+          onClick={() =>
+            setPlaybackState(playbackState === "PAUSED" ? "PLAYING" : "PAUSED")
+          }
+        >
+          {playbackState === "PLAYING" ? <MdPause /> : <MdPlayArrow />}
+        </div>
+        <div onClick={() => setPlaybackState("SKIPPING")}>
+          <MdSkipNext />
+        </div>
       </div>
-      <div onClick={() => setPlaybackState("SKIPPING")}>
-        <MdSkipNext />
-      </div>
-    </div>
+      <PitchControls disabled={disabled} />
+    </>
   );
 };
 
