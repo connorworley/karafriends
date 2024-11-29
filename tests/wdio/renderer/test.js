adfc38da46e268eaae5cf6f2472b2fa1ee8f257b
Connor Worley <cworley@netflix.com>
2024-11-29 14:05:26 -0800

    Give renderer integration tests some settle time before screenshotting

--- tests/wdio/renderer/test.js
+++ tests/wdio/renderer/test.js
@@ -3,6 +3,7 @@ describe("Electron tests", () => {
     await browser.url(
       `http://localhost:${process.env.KARAFRIENDS_DEV_PORT}/renderer/`,
     );
+    await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
     await browser.saveScreenshot("renderer.png");
   });
 });
