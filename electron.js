4ae0bb0cfb4c71f944fbb1b77b2cfecc951cc181
Connor Worley <connorbworley@gmail.com>
2025-02-01 02:29:46 -0800

    Use Yarn PnP (#1363)
    * Use Yarn PnP
    
    * Fix wdio

--- electron.js
+++ electron.js
@@ -1,17 +1,19 @@
-#!/usr/bin/env node
+#!/usr/bin/env yarn node
 const { spawnSync } = require("child_process");
 const { resolve } = require("path");
 
 const electron = require("electron");
 
-spawnSync(
+const { status } = spawnSync(
   electron,
   process.argv.slice(process.argv.findIndex((arg) => arg === __filename) + 1),
   {
     env: {
       ...process.env,
-      NODE_OPTIONS: `--enable-source-maps '--require=${resolve(__dirname, ".pnp.cjs")}'`,
+      NODE_OPTIONS: `--enable-source-maps '--require=${resolve(__dirname, ".pnp.cjs")}' '--experimental-loader=${resolve(__dirname, ".pnp.loader.mjs")}'`,
     },
     stdio: "inherit",
   },
 );
+console.log(`Electron process finished with status ${status}`);
+process.exit(status);
